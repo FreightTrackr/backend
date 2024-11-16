c2b1b48b309032e17d908583aa2544c1ade2cb8d
Ibrohim Mubarok <befousmain@gmail.com>
2024-11-16 17:38:28 +0700

    Buat main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/FreightTrackr/backend/config"
+	"github.com/FreightTrackr/backend/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	config.LoadEnv(".env")
+
+	app := fiber.New()
+
+	app.Use(cors.New(config.CorsAllowAll))
+
+	routes.UserRoute(app)
+	routes.TransaksiRoute(app)
+	routes.KantorRoute(app)
+	routes.PelangganRoute(app)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+}
