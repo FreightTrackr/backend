c480831cc253d6eeedb652b949ad9624bc48827d
Ibrohim Mubarok <befousmain@gmail.com>
2024-11-16 17:37:05 +0700

    Tambah util

---
+++ utils/transaksi.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"github.com/FreightTrackr/backend/helpers"
+	"github.com/FreightTrackr/backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func InsertTransaksi(mongoenv *mongo.Database, collname string, datatransaksi models.Transaksi) (interface{}, error) {
+	return helpers.InsertOneDoc(mongoenv, collname, datatransaksi)
+}
+
+func GetAllTransaksi(mongoenv *mongo.Database, collname string) ([]models.Transaksi, error) {
+	return helpers.GetAllDoc[models.Transaksi](mongoenv, collname)
+}
+
+func GetAllTransaksiWithPagination(mongoenv *mongo.Database, collname string, page, limit int) ([]models.Transaksi, models.DataCount, error) {
+	return helpers.GetDataForAnalyst[models.Transaksi](mongoenv, collname, page, limit)
+}
+
+func FindTransaksi(mongoenv *mongo.Database, collname string, datatransaksi models.Transaksi) models.Transaksi {
+	filter := bson.M{"no_resi": datatransaksi.No_Resi}
+	return helpers.GetOneDoc[models.Transaksi](mongoenv, collname, filter)
+}
+
+func TransaksiExists(mongoenv *mongo.Database, collname string, datatransaksi models.Transaksi) bool {
+	filter := bson.M{"no_resi": datatransaksi.No_Resi}
+	return helpers.DocExists[models.Transaksi](mongoenv, collname, filter, datatransaksi)
+}
+
+func UpdateTransaksi(mongoenv *mongo.Database, collname string, datatransaksi models.Transaksi) interface{} {
+	filter := bson.M{"no_resi": datatransaksi.No_Resi}
+	return helpers.ReplaceOneDoc(mongoenv, collname, filter, datatransaksi)
+}
+
+func DeleteTransaksi(mongoenv *mongo.Database, collname string, datatransaksi models.Transaksi) interface{} {
+	filter := bson.M{"no_resi": datatransaksi.No_Resi}
+	return helpers.DeleteOneDoc(mongoenv, collname, filter)
+}
