8824a66f1bd265c6130fecb99e12fd315bb6f713
Ibrohim Mubarok <befousmain@gmail.com>
2024-11-16 19:13:07 +0700

    Ganti LoadEnv

--- config/init.go
+++ config/init.go
@@ -1,14 +1,9 @@
 package config
 
 import (
-	"log"
-
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv(path string) {
-	// Load .env file
-	if err := godotenv.Load(path); err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load(path)
 }
