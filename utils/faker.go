9a4ff2c1bd36351f62e2363a23234d60a8e98a91
Ibrohim Mubarok <befousmain@gmail.com>
2024-11-26 13:20:45 +0700

    Buat faker jadi lebih akurat dengan desain data

--- utils/faker.go
+++ utils/faker.go
@@ -14,18 +14,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type DataAlamat struct {
-	Kode_Pos       int    `json:"kode_pos,omitempty" bson:"kode_pos,omitempty"`
-	Desa_Kelurahan string `json:"desa_kelurahan,omitempty" bson:"desa_kelurahan,omitempty"`
-	Kecamatan      string `json:"kecamatan,omitempty" bson:"kecamatan,omitempty"`
-	Kota_Kabupaten string `json:"kota_kabupaten,omitempty" bson:"kota_kabupaten,omitempty"`
-	Ibu_Kota       string `json:"ibu_kota,omitempty" bson:"ibu_kota,omitempty"`
-	Kode_Kota      string `json:"kode_kota,omitempty" bson:"kode_kota,omitempty"`
-	Kode_Bandara   string `json:"kode_bandara,omitempty" bson:"kode_bandara,omitempty"`
-	Provinsi       string `json:"provinsi,omitempty" bson:"provinsi,omitempty"`
-	Alamat_Lengkap string `json:"alamat_lengkap,omitempty" bson:"alamat_lengkap,omitempty"`
-}
-
 func DummyUserGenerator(n int, mconn *mongo.Database) (string, error) {
 
 	for i := 0; i < n; i++ {
@@ -67,16 +55,60 @@ func DummyUserGenerator(n int, mconn *mongo.Database) (string, error) {
 	return "Bergasil generate " + strconv.Itoa(n) + " data", nil
 }
 
-func DummyTransaksiGenerator(n int, mconn *mongo.Database) (string, error) {
+func generateRandomDate(start time.Time, end time.Time) time.Time {
+	diff := end.Sub(start)
+	randDuration := time.Duration(rand.Int63n(int64(diff)))
+	return start.Add(randDuration)
+}
 
+func DummyTransaksiGenerator(n int, mconn *mongo.Database) (string, error) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	layanan := []string{"Nextday", "Reguler"}
+	isi_kiriman := []string{"Dokumen", "Paket"}
+	tipe_cod := []string{"cod", "nonCod"}
+	status_cod := []string{"sudah_setor", "belum_setor"}
+	status := []string{"delivered", "canceled", "returned", "inWarehouse", "inVehicle", "failed", "paid"}
+	startDate := time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC)
+	endDate := time.Now()
 	for i := 0; i < n; i++ {
 		var transaksi models.Transaksi
+
 		alamatpengirim := generator.GenerateRandomAlamat()
 		alamatpenerima := generator.GenerateRandomAlamat()
 
-		transaksi.No_Resi = "P" + strconv.Itoa(rand.Intn(100000000000))
-		transaksi.Layanan = randomLayanan()
-		transaksi.Isi_Kiriman = faker.Word()
+		data_status := status[r.Intn(len(status))]
+		data_tipe_cod := tipe_cod[r.Intn(len(tipe_cod))]
+		tanggal_kirim := generateRandomDate(startDate, endDate)
+		transaksi.Tanggal_Kirim = primitive.NewDateTimeFromTime(tanggal_kirim)
+		transaksi.Sla = rand.Intn(4) + 2
+
+		if data_status == "delivered" {
+			var tanggal_antaran_pertama, tanggal_terima time.Time
+			tanggal_antaran_pertama = tanggal_kirim.Add(time.Duration(rand.Intn(4)+3) * 24 * time.Hour)
+			tanggal_terima = tanggal_antaran_pertama.Add(time.Duration(rand.Intn(3)) * 24 * time.Hour)
+			transaksi.Tanggal_Antaran_Pertama = primitive.NewDateTimeFromTime(tanggal_antaran_pertama)
+			transaksi.Tanggal_Terima = primitive.NewDateTimeFromTime(tanggal_terima)
+			transaksi.Aktual_Sla = int(tanggal_antaran_pertama.Sub(tanggal_kirim).Hours() / 24)
+			transaksi.Status_Sla = transaksi.Aktual_Sla <= transaksi.Sla
+		}
+		if data_tipe_cod == "cod" {
+			transaksi.Status_Cod = status_cod[r.Intn(len(status_cod))]
+		}
+
+		year := tanggal_kirim.Year()
+		data_layanan := layanan[r.Intn(len(layanan))]
+		data_isi_kiriman := isi_kiriman[r.Intn(len(isi_kiriman))]
+		data_berat_kiriman := float64(rand.Intn(100)) + (float64(rand.Intn(9)+1) / 10)
+
+		data_volumetrik := float64(rand.Intn(491)+10) + float64(rand.Intn(10))/10.0
+
+		base := (rand.Intn(90) + 10) * 1000
+		extra := []int{0, 500}[rand.Intn(2)]
+		data_biaya_dasar := base + extra
+
+		transaksi.No_Resi = "P" + strconv.Itoa(year)[2:] + strconv.Itoa(rand.Intn(10000000))
+		transaksi.Layanan = data_layanan
+		transaksi.Isi_Kiriman = data_isi_kiriman
 		transaksi.Nama_Pengirim = faker.Name()
 		transaksi.Alamat_Pengirim = alamatpengirim.Alamat_Lengkap
 		transaksi.Kode_Pos_Pengirim = alamatpengirim.Kode_Pos
@@ -85,22 +117,22 @@ func DummyTransaksiGenerator(n int, mconn *mongo.Database) (string, error) {
 		transaksi.Alamat_Penerima = alamatpenerima.Alamat_Lengkap
 		transaksi.Kode_Pos_Penerima = alamatpenerima.Kode_Pos
 		transaksi.Kota_Tujuan = alamatpenerima.Kota_Kabupaten
-		transaksi.Berat_Kiriman = rand.Float64() * 10 // Random weight between 0 and 10 kg
-		transaksi.Volumetrik = rand.Float64() * 500   // Random volumetrik between 0 and 500
-		transaksi.Nilai_Barang = rand.Intn(10000000)  // Random value between 0 and 10 million
-		transaksi.Biaya_Dasar = rand.Intn(100000)     // Random base cost between 0 and 100 thousand
-		transaksi.Biaya_Pajak = int(float64(transaksi.Biaya_Dasar) * 0.11)
-		transaksi.Biaya_Asuransi = int(float64(transaksi.Nilai_Barang) * 0.005)
+		transaksi.Berat_Kiriman = data_berat_kiriman
+		transaksi.Volumetrik = data_volumetrik
+		transaksi.Biaya_Dasar = data_biaya_dasar
+		transaksi.Biaya_Pajak = int(float64(data_biaya_dasar) * 0.11)
+
+		if rand.Intn(2) == 0 {
+			transaksi.Nilai_Barang = 0
+			transaksi.Biaya_Asuransi = 0
+		} else {
+			transaksi.Nilai_Barang = (100 + rand.Intn(901)) * 1000
+			transaksi.Biaya_Asuransi = int(float64(transaksi.Nilai_Barang) * 0.005)
+		}
+
 		transaksi.Total_Biaya = transaksi.Biaya_Dasar + transaksi.Biaya_Pajak + transaksi.Biaya_Asuransi
-		transaksi.Tanggal_Kirim = primitive.NewDateTimeFromTime(time.Now())
-		transaksi.Tanggal_Terima = primitive.NewDateTimeFromTime(time.Now().AddDate(0, 0, 1))
-		transaksi.Tanggal_Tenggat = primitive.NewDateTimeFromTime(time.Now().AddDate(0, 0, 1))
-		transaksi.Status = randomStatus()
-		transaksi.Tipe_Cod = randomTipeCod()
-		transaksi.Status_Cod = randomStatusCod()
-		transaksi.Sla = 1
-		transaksi.Aktual_Sla = 1
-		transaksi.Status_Sla = true
+		transaksi.Status = data_status
+		transaksi.Tipe_Cod = data_tipe_cod
 		transaksi.No_Pend_Kirim = fakeDigit()
 		transaksi.No_Pend_Terima = fakeDigit()
 		transaksi.Kode_Pelanggan = faker.Username()
@@ -143,7 +175,7 @@ func DummyKantorGenerator(n int, mconn *mongo.Database) (string, error) {
 }
 
 func DummyPelangganGenerator(n int, mconn *mongo.Database) (string, error) {
-	tipePelangganOptions := []string{"Retail", "Corporate", "Marketplace"}
+	tipePelangganOptions := []string{"Corporate", "Marketplace"}
 	marketplaceNames := []string{"Shopee", "Tokopedia", "Bukalapak", "Lazada"}
 	corporateNames := []string{"Mandiri", "BRI", "BCA", "CitiBank"}
 
@@ -192,29 +224,9 @@ func DummyHistoryGenerator(n int, mconn *mongo.Database) (string, error) {
 	return "Berhasil generate " + strconv.Itoa(n) + " data", nil
 }
 
-func randomLayanan() string {
-	layanan := []string{"Reguler", "Cepat", "Express"}
-	return layanan[rand.Intn(len(layanan))]
-}
-
 func randomStatus() string {
 	status := []string{"delivered", "canceled", "returned", "inWarehouse", "inVehicle", "failed"}
 	return status[rand.Intn(len(status))]
-}
-
-func randomTipeCod() string {
-	tipeCod := []string{"no cod", "cod"}
-	return tipeCod[rand.Intn(len(tipeCod))]
-}
-
-func randomStatusCod() string {
-	statusCod := []string{"paid", "unpaid", "onProcess"}
-	return statusCod[rand.Intn(len(statusCod))]
-}
-
-// Fungsi untuk menghasilkan kode pos palsu
-func fakeZipCode() int {
-	return rand.Intn(99999-10000) + 10000
 }
 
 // Fungsi untuk menghasilkan digit palsu
