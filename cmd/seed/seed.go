9b7b2681e8befd8e8d1771f00cb3b3bba2882fc5
Ibrohim Mubarok <befousmain@gmail.com>
2024-11-26 13:21:23 +0700

    Rubah seed

--- cmd/seed/seed.go
+++ cmd/seed/seed.go
@@ -11,24 +11,24 @@ func main() {
 	config.LoadEnv(".env")
 
 	mconn := utils.SetConnection()
-	_, err := utils.DummyUserGenerator(179, mconn)
-	if err != nil {
-		log.Fatalf("Error generating dummy users: %v", err)
-	}
-	// _, err = utils.DummyTransaksiGenerator(50, mconn)
+	// _, err := utils.DummyUserGenerator(179, mconn)
 	// if err != nil {
 	// 	log.Fatalf("Error generating dummy users: %v", err)
 	// }
+	_, err := utils.DummyTransaksiGenerator(190000, mconn)
+	if err != nil {
+		log.Fatalf("Error generating dummy transaksi: %v", err)
+	}
 	// _, err = utils.DummyKantorGenerator(20, mconn)
 	// if err != nil {
-	// 	log.Fatalf("Error generating dummy users: %v", err)
+	// 	log.Fatalf("Error generating dummy kantor: %v", err)
 	// }
 	// _, err = utils.DummyPelangganGenerator(100, mconn)
 	// if err != nil {
-	// 	log.Fatalf("Error generating dummy users: %v", err)
+	// 	log.Fatalf("Error generating dummy pelanggan: %v", err)
 	// }
 	// _, err = utils.DummyHistoryGenerator(30, mconn)
 	// if err != nil {
-	// 	log.Fatalf("Error generating dummy users: %v", err)
+	// 	log.Fatalf("Error generating dummy history: %v", err)
 	// }
 }
