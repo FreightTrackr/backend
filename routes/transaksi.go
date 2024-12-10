cc255ae0de250e752df9f12f819733f4dcd54d37
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-10 16:00:56 +0700

    Ganti nama endpoint

--- routes/transaksi.go
+++ routes/transaksi.go
@@ -10,7 +10,7 @@ func TransaksiRoute(page *fiber.App) {
 	page.Use(config.JwtMiddleware())
 	page.Get("/transaksi", controllers.AmbilSemuaTransaksiDenganPagination)
 	page.Get("/semuatransaksi", controllers.AmbilSemuaTransaksi)
-	page.Get("/transaksislatrue", controllers.AmbilTransaksiDenganStatusDelivered)
+	page.Get("/transaksistatusdelivered", controllers.AmbilTransaksiDenganStatusDelivered)
 	page.Post("/transaksi", controllers.TambahTransaksi)
 	page.Delete("/transaksi", controllers.HapusTransaksi)
 }
