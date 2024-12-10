990a6ade066be95e785b795f7c4f708c30bb14f0
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-10 15:44:14 +0700

    Hapus logika yang membuat menampilkan semua data jika tanggal dikosongkan

--- utils/parse.go
+++ utils/parse.go
@@ -3,12 +3,12 @@ package utils
 import "time"
 
 func ParseDate(dateStr string, isEndDate bool) (time.Time, error) {
-	if dateStr == "" {
-		if isEndDate {
-			return time.Now(), nil
-		}
-		return time.Time{}, nil
-	}
+	// if dateStr == "" {
+	// 	if isEndDate {
+	// 		return time.Now(), nil
+	// 	}
+	// 	return time.Time{}, nil
+	// }
 
 	date, err := time.Parse(time.RFC3339, dateStr)
 	if err != nil {
