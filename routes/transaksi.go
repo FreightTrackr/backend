7fadc78df9f450bb7c6282b2182a7af919ab9254
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-10 15:57:01 +0700

    Tambah endpoint transaksislatrue

--- routes/transaksi.go
+++ routes/transaksi.go
@@ -10,6 +10,7 @@ func TransaksiRoute(page *fiber.App) {
 	page.Use(config.JwtMiddleware())
 	page.Get("/transaksi", controllers.AmbilSemuaTransaksiDenganPagination)
 	page.Get("/semuatransaksi", controllers.AmbilSemuaTransaksi)
+	page.Get("/transaksislatrue", controllers.AmbilTransaksiDenganStatusSlaTrue)
 	page.Post("/transaksi", controllers.TambahTransaksi)
 	page.Delete("/transaksi", controllers.HapusTransaksi)
 }
