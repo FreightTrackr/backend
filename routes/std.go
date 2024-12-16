9e214dda10da1ba09b123aa830f79d89e11c1a70
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-17 02:15:15 +0700

    Tambahkan route session

--- routes/std.go
+++ routes/std.go
@@ -11,4 +11,5 @@ func StdRoute(router *http.ServeMux) {
 	router.Handle("POST /std/register", config.IsAuthenticated(http.HandlerFunc(controllers.StdRegister)))
 	router.HandleFunc("POST /std/login", controllers.StdLogin)
 	router.Handle("GET /std/users", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaUser)))
+	router.Handle("GET /std/session", config.IsAuthenticated(http.HandlerFunc(controllers.StdSession)))
 }
