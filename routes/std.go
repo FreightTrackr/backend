924b1639c5ea225499df89dcaa9d8dda2bade0f8
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-17 02:23:38 +0700

    Tambahkan route pelanggan

--- routes/std.go
+++ routes/std.go
@@ -13,4 +13,5 @@ func StdRoute(router *http.ServeMux) {
 	router.Handle("GET /std/users", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaUser)))
 	router.Handle("GET /std/session", config.IsAuthenticated(http.HandlerFunc(controllers.StdSession)))
 	router.Handle("GET /std/kantor", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaKantor)))
+	router.Handle("GET /std/pelanggan", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaPelanggan)))
 }
