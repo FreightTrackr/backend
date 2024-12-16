12b41138eaa57ac21a0788c8ebb3f05e61fdaa8b
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-16 21:17:49 +0700

    Tambahkan middleware untuk register

--- routes/std.go
+++ routes/std.go
@@ -3,10 +3,11 @@ package routes
 import (
 	"net/http"
 
+	"github.com/FreightTrackr/backend/config"
 	"github.com/FreightTrackr/backend/controllers"
 )
 
 func StdRoute(router *http.ServeMux) {
-	router.HandleFunc("POST /std/register", controllers.StdRegister)
+	router.Handle("/std/register", config.IsAuthenticated(http.HandlerFunc(controllers.StdRegister)))
 	router.HandleFunc("POST /std/login", controllers.StdLogin)
 }
