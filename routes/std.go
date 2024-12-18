12fc08894c5bd13683a943502986f0e96013ad16
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-18 22:14:12 +0700

    Ganti agar routing standard library menggunakan middleware bukan config

--- routes/std.go
+++ routes/std.go
@@ -3,19 +3,19 @@ package routes
 import (
 	"net/http"
 
-	"github.com/FreightTrackr/backend/config"
 	"github.com/FreightTrackr/backend/controllers"
+	"github.com/FreightTrackr/backend/middleware"
 )
 
 func StdRoute(router *http.ServeMux) {
-	router.Handle("POST /std/register", config.IsAuthenticated(http.HandlerFunc(controllers.StdRegister)))
+	router.Handle("POST /std/register", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdRegister)))
 	router.HandleFunc("POST /std/login", controllers.StdLogin)
-	router.Handle("GET /std/users", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaUser)))
-	router.Handle("GET /std/session", config.IsAuthenticated(http.HandlerFunc(controllers.StdSession)))
-	router.Handle("GET /std/kantor", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaKantor)))
-	router.Handle("GET /std/pelanggan", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaPelanggan)))
-	router.Handle("GET /std/transaksi", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaTransaksiDenganPagination)))
-	router.Handle("GET /std/semuatransaksi", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaTransaksi)))
-	router.Handle("GET /std/transaksidelivered", config.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilTransaksiDenganStatusDelivered)))
-	router.Handle("GET /std/getrole", config.IsAuthenticated(http.HandlerFunc(controllers.StdGetRole)))
+	router.Handle("GET /std/users", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaUser)))
+	router.Handle("GET /std/session", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdSession)))
+	router.Handle("GET /std/kantor", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaKantor)))
+	router.Handle("GET /std/pelanggan", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaPelanggan)))
+	router.Handle("GET /std/transaksi", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaTransaksiDenganPagination)))
+	router.Handle("GET /std/semuatransaksi", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaTransaksi)))
+	router.Handle("GET /std/transaksidelivered", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilTransaksiDenganStatusDelivered)))
+	router.Handle("GET /std/getrole", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdGetRole)))
 }
