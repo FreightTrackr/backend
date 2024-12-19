dadc0d8d1f0c67e85baf7e746c914969c356f113
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-19 22:22:37 +0700

    Update seed

--- cmd/seed/seed.go
+++ cmd/seed/seed.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/FreightTrackr/backend/config"
+	"github.com/FreightTrackr/backend/middleware"
 	"github.com/FreightTrackr/backend/utils"
 )
 
 func main() {
-	config.LoadEnv(".env")
+	middleware.LoadEnv(".env")
 
 	mconn := utils.SetConnection()
 	// _, err := utils.DummyUserGenerator(179, mconn)
@@ -19,14 +19,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error generating dummy transaksi: %v", err)
 	}
-	_, err = utils.DummyKantorGenerator(mconn)
-	if err != nil {
-		log.Fatalf("Error generating dummy kantor: %v", err)
-	}
-	_, err = utils.DummyPelangganGenerator(mconn)
-	if err != nil {
-		log.Fatalf("Error generating dummy pelanggan: %v", err)
-	}
+	// _, err = utils.DummyKantorGenerator(mconn)
+	// if err != nil {
+	// 	log.Fatalf("Error generating dummy kantor: %v", err)
+	// }
+	// _, err = utils.DummyPelangganGenerator(mconn)
+	// if err != nil {
+	// 	log.Fatalf("Error generating dummy pelanggan: %v", err)
+	// }
 	// _, err = utils.DummyHistoryGenerator(30, mconn)
 	// if err != nil {
 	// 	log.Fatalf("Error generating dummy history: %v", err)
