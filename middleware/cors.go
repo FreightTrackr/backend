eabeee0f883b1d23dc4d38111ecf2edf07dc3495
Ibrohim Mubarok <befousmain@gmail.com>
2025-01-02 12:25:29 +0700

    Tambah origin

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,6 +9,7 @@ import (
 
 var origins = []string{
 	"https://freighttrackr.github.io",
+	"https://freighttrackr.befous.com",
 }
 
 var headers = []string{
@@ -16,12 +17,6 @@ var headers = []string{
 	"Content-Type",
 	"Accept",
 	"Authorization",
-	"Access-Control-Request-Headers",
-	"Token",
-	"Login",
-	"Access-Control-Allow-Origin",
-	"Bearer",
-	"X-Requested-With",
 }
 
 var Cors = cors.Config{
