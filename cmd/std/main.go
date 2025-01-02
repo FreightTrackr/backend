b921f3b69845b37abab125468130f63ec7b01b57
Ibrohim Mubarok <befousmain@gmail.com>
2025-01-02 12:28:42 +0700

    Gunakan StdCors

--- cmd/std/main.go
+++ cmd/std/main.go
@@ -14,7 +14,7 @@ func main() {
 	routes.StdRoute(app)
 	server := http.Server{
 		Addr:    ":3000",
-		Handler: app,
+		Handler: middleware.StdCors(app),
 	}
 	fmt.Println("Starting server on port :3000")
 	server.ListenAndServe()
