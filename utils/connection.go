6eea72693712a3356f0ff8027486e8d06e192b61
Ibrohim Mubarok <befousmain@gmail.com>
2025-01-03 09:20:04 +0700

    Buat agar koneksi hanya sekali

--- utils/connection.go
+++ utils/connection.go
@@ -2,16 +2,26 @@ package utils
 
 import (
 	"os"
+	"sync"
 
 	"github.com/FreightTrackr/backend/helpers"
 	"github.com/FreightTrackr/backend/models"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+var dbInstance *mongo.Database
+var once sync.Once
+
 func SetConnection() *mongo.Database {
-	var DBmongoinfo = models.DBInfo{
-		DBString: os.Getenv("DB_URI"),
-		DBName:   os.Getenv("DB_DATABASE"),
+	once.Do(func() {
+		dbInfo := models.DBInfo{
+			DBString: os.Getenv("DB_URI"),
+			DBName:   os.Getenv("DB_DATABASE"),
+		}
+		dbInstance = helpers.MongoConnect(dbInfo)
+	})
+	if dbInstance == nil {
+		return nil
 	}
-	return helpers.MongoConnect(DBmongoinfo)
+	return dbInstance
 }
