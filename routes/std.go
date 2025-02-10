07d28a9586aed5363509f1384027b356c4b6ff82
Ibrohim Mubarok <befousmain@gmail.com>
2025-02-11 00:41:24 +0700

    Buat agar register tidak perlu autentikasi

--- routes/std.go
+++ routes/std.go
@@ -8,7 +8,7 @@ import (
 )
 
 func StdRoute(router *http.ServeMux) {
-	router.Handle("POST /register", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdRegister)))
+	router.HandleFunc("POST /register", controllers.StdRegister)
 	router.HandleFunc("POST /login", controllers.StdLogin)
 	router.Handle("GET /users", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdAmbilSemuaUser)))
 	router.Handle("PUT /users", middleware.IsAuthenticated(http.HandlerFunc(controllers.StdEditUser)))
